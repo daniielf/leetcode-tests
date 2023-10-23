be829ff910c9f93281b506133294d95e04c559ba
daniielf <danieldefreitas@grover.com>
2023-10-23 22:46:47 +0200

    easy: is tere symmetric

---
+++ easy/is-tree-symmetric/isTreeSymmetric.js
@@ -0,0 +1,13 @@
+var isSymmetric = function(root) {
+  if (!root) { return true };
+
+  const isMirror = (left, right) => {
+      if (left === null && right === null) return true;
+      if (left === null || right === null) return false;
+      return (left.val === right.val) && 
+      isMirror(left.left, right.right) && 
+      isMirror(left.right, right.left);
+  }
+
+  return isMirror(root.left, root.right);
+};
